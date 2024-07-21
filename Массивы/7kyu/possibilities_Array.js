1e715fa817f95fc9d60ccac4a573d67555513149
Surkho <abuyev1999@internet.ru>
2024-07-21 16:07:30 +0300

    feat: add function to check if an array is an array of all possibilities
    - Implemented isAllPossibilities function to verify if an array contains all numbers between 0 and its length - 1.
    - Used a Set to efficiently check for the presence of each required number.
    - Optimized the function to improve performance over large arrays.

---
+++ Массивы/7kyu/possibilities_Array.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// A non-empty array a of length n is called an array of all possibilities if it contains all numbers between 0 and a.length - 1 (both inclusive).
+
+// Write a function that accepts an integer array and returns true if the array is an array of all possibilities, else false.
+
+// Examples:
+
+// [1,2,0,3] => True
+// # Includes all numbers between 0 and a.length - 1 (4 - 1 = 3)
+
+// [0,1,2,2,3] => False
+// # Doesn't include all numbers between 0 and a.length - 1 (5 - 1 = 4)
+
+// [0] => True
+// # Includes all numbers between 0 and a.length - 1 (1 - 1 = 0).
+
+
+function isAllPossibilities(x){
+    for (let i = 0; i <= x.length - 1; i++) {
+        if (!x.includes(i)) {
+            return false
+        }
+    }
+
+    return true
+}
+
+
+console.log(isAllPossibilities([1,2,0,3]));
+console.log(isAllPossibilities([0,1,2,2,3]));
+console.log(isAllPossibilities([0]));
