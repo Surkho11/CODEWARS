06685ccfc6ea884122c7806921ce355177714d39
Surkho <abuyev1999@internet.ru>
2024-07-24 13:06:08 +0300

    Fix parseNumbers function to correctly convert string array to number array
    - Updated parseNumbers function to use map with parseInt for converting each string in the array to an integer.
    - Ensured that each string is correctly parsed to an integer using parseInt within the map function.
    
    This update correctly transforms an array of numeric strings into an array of numbers.

---
+++ Массивы/7kyu/parse_integers_in_array.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/*
+A colleague asked if you can help him fix his function. It seems it doesn't always parse Integers correctly.
+
+Examples:
+
+parseNumbers(['10']) // should return [10]
+parseNumbers(['-1','0','1']) // should return [-1,0,1]
+*/
+
+
+var parseNumbers = function(intStrs) {
+    return intStrs.map(num => parseInt(num));
+}
+
+console.log(parseNumbers(['10']));
+console.log(parseNumbers(['-1','0','1']));
