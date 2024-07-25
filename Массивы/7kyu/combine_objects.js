256d8b73cb2b9528c7c61259877224f311f1cbba
Surkho <abuyev1999@internet.ru>
2024-07-25 20:05:45 +0300

    feat: Add combine function to merge multiple objects by summing their values
    - Implemented the combine function that takes two or more objects as input.
    - The function returns a new object with properties from all input objects.
    - For matching keys, the function sums the values.
    - Ensures the original input objects are not mutated.
    - Added example objects objA, objB, objC, and objD to demonstrate usage.

---
+++ Массивы/7kyu/combine_objects.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/*
+   Your task is to write a function that takes two or more objects and returns a new object which combines all the input objects.
+
+All input object properties will have only numeric values. Objects are combined together so that the values of matching keys are added together.
+
+An example:
+
+const objA = { a: 10, b: 20, c: 30 }
+const objB = { a: 3, c: 6, d: 3 }
+combine(objA, objB) // Returns { a: 13, b: 20, c: 36, d: 3 }
+The combine function should be a good citizen, so should not mutate the input objects.
+
+
+*/
+
+const objA = { a: 10, b: 20, c: 30 }
+const objB = { a: 3, c: 6, d: 3 }
+const objC = { a: 5, d: 11, e: 8 }
+const objD = { c: 3 }
+
+function combine(...obj) {
+  return objs.reduce((acc, obj) => {
+    for (const key in obj) {
+      if (acc.hasOwnProperty(key)) {
+        acc[key] += obj[key];
+      } else {
+        acc[key] = obj[key];
+      }
+    }
+    return acc;
+  }, {});
+}
+
+console.log(objA, objB, objC, objD);
