f6ec95a74f5d652accdcd2dcd0916056bbd44a69
Surkho <abuyev1999@internet.ru>
2024-07-28 20:37:13 +0300

    Add nameShuffler function to swap first and last names
    - Implemented nameShuffler function to split a full name by spaces, reverse the order of the names, and join them back into a string.

---
+++ Строки/8kyu/Name_Shuffler.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*
+Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"
+*/
+
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+}
+
+console.log(nameShuffler('john McClane'));
+console.log(nameShuffler('Mary jeggins'));
+console.log(nameShuffler('tom jerry'));
