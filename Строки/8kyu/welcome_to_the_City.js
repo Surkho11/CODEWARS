d4d9c096698a1fb61e5801c6bd49bc5617927cea
Surkho <abuyev1999@internet.ru>
2024-07-29 12:25:23 +0300

    Implement greeting function
    - Added sayHello function to welcome a person.
    - The function takes an array of name components, a city, and a state.
    - Joins name components with a space and formats the greeting message.

---
+++ Строки/8kyu/welcome_to_the_City.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/*
+Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+Example:
+
+['John', 'Smith'], 'Phoenix', 'Arizona'
+This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+*/
+
+function sayHello(name, city, state) {
+  return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
+}
+
+console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'));
+console.log(
+  sayHello(['Franklin', 'Delano', 'Roosevelt'], 'Chicago', 'Illinois')
+);
+console.log(sayHello(['Wallace', 'Russel', 'Osbourne'], 'Albany', 'New York'));
