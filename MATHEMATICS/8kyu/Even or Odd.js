e3f80bf1683cd4a3e37b87326ecc0b0c926912fa
Surkho <abuyev1999@internet.ru>
2024-07-30 16:41:09 +0300

    Implement evenOrOdd function to determine if a number is even or odd
    - Created a function evenOrOdd that takes an integer as an argument.
    - The function uses the modulus operator to check if the number is even or odd.
    - Returns 'Even' if the number is even, otherwise returns 'Odd'.
    - Ensures the function handles both positive and negative integers as well as zero.

---
+++ MATHEMATICS/8kyu/Even or Odd.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/*
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+function evenOrOdd(number) {
+  return number % 2 === 0 ? 'Even' : 'Odd';
+}
+
+
+console.log(evenOrOdd(2));
+console.log(evenOrOdd(7));
