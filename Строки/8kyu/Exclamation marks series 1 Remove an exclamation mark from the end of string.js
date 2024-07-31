a9b31d058d31085c0bb415909d3031db0781ae0e
Surkho <abuyev1999@internet.ru>
2024-07-31 11:05:04 +0300

    feat: add function to remove exclamation mark from the end of a string
    Added a function 'remove' that takes a string as input and removes the exclamation mark from the end if it is present.

---
+++ Строки/8kyu/Exclamation marks series 1 Remove an exclamation mark from the end of string.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+Examples
+"Hi!"     ---> "Hi"
+"Hi!!!"   ---> "Hi!!"
+"!Hi"     ---> "!Hi"
+"!Hi!"    ---> "!Hi"
+"Hi! Hi!" ---> "Hi! Hi"
+"Hi"      ---> "Hi"
+*/
+
+function remove(string) {
+  return string[string.length - 1] === '!'
+    ? string.slice(0, string.length - 1)
+    : string;
+}
+
+console.log(remove('Hi!'));
+console.log(remove('Hi!!!'));
+console.log(remove('!Hi'));
+console.log(remove('!Hi!'));
+console.log(remove('Hi! Hi!'));
+console.log(remove('Hi'));
