d7098ba26a17cd29ee12ef3f6b04a5cc3a414ac6
Surkho <abuyev1999@internet.ru>
2024-07-31 18:48:50 +0300

    feat: add function to check if developers from all age groups have signed up
    - Implemented the isAgeDiverse function to verify the presence of developers from each age group.
    - Created an object to track the count of developers in each age group.
    - Used conditional statements to increment the appropriate age group counters.
    - Checked that all age groups have at least one developer by using Object.values and Array.every.

---
+++ Массивы/6kyu/Coding Meetup #9 - Higher-Order Functions Series - Is the meetup age-diverse.js
@@ -0,0 +1,85 @@
+'use strict';
+
+/*
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if developers from all of the following age groups have signed up: teens, twenties, thirties, forties, fifties, sixties, seventies, eighties, nineties, centenarian (at least 100 years young).
+false otherwise.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 19, language: 'Python' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+  { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 40, language: 'Ruby' },
+  { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 59, language: 'C' },
+  { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 60, language: 'C' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 75, language: 'Python' },
+  { firstName: 'Chloe', lastName: 'K.', country: 'Guernsey', continent: 'Europe', age: 88, language: 'Ruby' },
+  { firstName: 'Viktoria', lastName: 'W.', country: 'Bulgaria', continent: 'Europe', age: 98, language: 'PHP' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript' }
+];
+your function should return true as there is at least one developer from each age group.
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+Age is represented by a number which can be any positive integer up to 199.
+*/
+
+var list1 = [
+    { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 19, language: 'Python' },
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+    { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+    { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 40, language: 'Ruby' },
+    { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 59, language: 'C' },
+    { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 60, language: 'C' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 75, language: 'Python' },
+    { firstName: 'Chloe', lastName: 'K.', country: 'Guernsey', continent: 'Europe', age: 88, language: 'Ruby' },
+    { firstName: 'Viktoria', lastName: 'W.', country: 'Bulgaria', continent: 'Europe', age: 98, language: 'PHP' },
+    { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript' }
+  ];
+
+function isAgeDiverse(list) {
+    const obj = {
+        teens: 0,
+        twenties: 0,
+        thirties: 0,
+        forties: 0,
+        fifties: 0,
+        sixties: 0,
+        seventies: 0,
+        eighties: 0,
+        nineties: 0,
+        centenarian: 0
+    }
+
+    for (const {age} of list) {
+        if (age < 20) {
+            obj.teens++
+        } else if (age >= 20 && age < 30) {
+            obj.twenties++
+        } else if (age >= 30 && age < 40) {
+            obj.thirties++
+        } else if (age >= 40 && age < 50) {
+            obj.forties++
+        } else if (age >= 50 && age < 60) {
+            obj.fifties++
+        } else if (age >= 60 && age < 70) {
+            obj.sixties++
+        } else if (age >= 70 && age < 80) {
+            obj.seventies++
+        } else if (age >= 80 && age < 90) {
+            obj.eighties++
+        } else if (age >= 90 && age < 100) {
+            obj.nineties++
+        } else{
+            obj.centenarian++
+        }
+    }
+    return Object.values(obj).every(num => num > 0);   
+}
+
+console.log(isAgeDiverse(list1));
