394c0cc287e55ad3ed902be599b1619e1374462c
Surkho <abuyev1999@internet.ru>
2024-07-31 18:14:01 +0300

    Implement smartSum function to handle nested arrays
    - Collect all arguments using the rest parameter.
    - Use lat(Infinity) to flatten nested arrays.
    - Use educe to calculate the sum of all elements.

---
+++ Массивы/6kyu/Smart Sum.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+/*
+Your task is to write a function, smartSum, that returns the sum of an arbitrary number of arguments. But be careful, some of your arguments can be arrays of numbers or nested arrays.
+
+smartSum(1,2,3,[4,5],6); // returns 21
+smartSum(1,2,[[3,4],5],6); // returns 21
+*/
+
+function smartSum(...arr){
+    return arr.flat(Infinity).reduce((acc, num) => acc + num, 0)
+}
