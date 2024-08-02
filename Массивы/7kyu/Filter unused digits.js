6b31ecdd1907c652f6d90680aa894264012d2312
Surkho <abuyev1999@internet.ru>
2024-08-02 13:49:26 +0300

    unusedDigits function to correctly identify missing digits

---
+++ Массивы/7kyu/Filter unused digits.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*
+Given a varying number of integer arguments, return the digits that are not present in any of them.
+
+Example:
+
+[12, 34, 56, 78]  =>  "09"
+[2015, 8, 26]     =>  "3479"
+Note: the digits in the resulting string should be sorted.
+*/
+
+function unusedDigits(...args) {
+  let numStr = '';
+
+  for (let i = 0; i < 10; i++) {
+    if (!args.join('').includes(i)) {
+      numStr += i;
+    }
+  }
+
+  return numStr;
+}
+
+console.log(unusedDigits(12, 34, 56, 78));
