728844bfcbd8fc179d80650c272453d7f957f8bc
Surkho <abuyev1999@internet.ru>
2024-08-03 12:14:54 +0300

    htmlspecialchars function to properly escape HTML characters
    Updated the htmlspecialchars function to correctly replace HTML special characters in user input.

---
+++ Строки/8kyu/u Safen User Input Part I - htmlspecialchars.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function htmlspecialchars(formData) {
+  return formData
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+}
