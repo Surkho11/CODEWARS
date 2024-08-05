a9cf7dba6a1136f6de3827517477b3d41a3ca203
Surkho <abuyev1999@internet.ru>
2024-08-05 20:34:28 +0300

    Implement removeRotten function
    - Check if the input array is null, undefined, or empty. If so, return an empty array.
    - Iterate through the array and replace rotten in the fruit names with an empty string.
    - Convert the resulting fruit names to lowercase.
    - Return the modified array.

---
+++ Массивы/7kyu/Help the Fruit Guy.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/*
+Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits should returns an array of strings where all the rotten fruits are replaced by good ones.
+
+Notes
+If the array is null/nil/None or empty you should return empty array ([]).
+The rotten fruit name will be in this camelcase (rottenFruit).
+The returned array should be in lowercase.
+*/
+
+function removeRotten(bagOfFruits) {
+  if (!Boolean(bagOfFruits)) return [];
+
+  return bagOfFruits.map((fruit) =>
+    fruit.includes('rotten')
+      ? fruit.replace(/rotten/g, '').toLowerCase()
+      : fruit
+  );
+}
+
+console.log(removeRotten(['apple', 'banana', 'kiwi', 'melone', 'orange']));
+
+console.log('rottenBanana'.includes('rotten'));
