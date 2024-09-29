230dae01bf1e29291078e6477ab468acba648d72
Surkho <abuyev1999@internet.ru>
2024-09-29 16:52:41 +0300

    Added a function that returns another string, such that the even and odd characters are grouped, and the groups are separated by spaces. The even group comes first, followed by a space, and then the odd part.

---
+++ Строки/7kyu/Odd-Even String Sort.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/*
+Given a string s, your task is to return another string such that even-indexed and odd-indexed characters of s are grouped and the groups are space-separated. Even-indexed group comes as first, followed by a space, and then by the odd-indexed part.
+
+Examples
+input:    "CodeWars" => "CdWr oeas"
+           ||||||||      |||| ||||
+indices:   01234567      0246 1357
+Even indices 0, 2, 4, 6, so we have "CdWr" as the first group.
+Odd indices are 1, 3, 5, 7, so the second group is "oeas".
+And the final string to return is "Cdwr oeas".
+*/
+
+function sortMyString(S) {
+  return `${S.split('')
+    .filter((el, i) => i % 2 === 0)
+    .join('')} ${S.split('')
+    .filter((el, i) => i % 2 !== 0)
+    .join('')}`;
+}
+
+console.log(sortMyString('CodeWars'));
